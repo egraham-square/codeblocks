5d5527078266223683abf757e2a431792a4b3c2e
Liz Graham <egraham@squareup.com>
2024-06-10 17:26:34 -0400

    Update js1.7

--- src/test/solutions/JS1.07.StringOperations2Solution.js
+++ src/test/solutions/JS1.07.StringOperations2Solution.js
@@ -6,7 +6,7 @@ String Length
 1.1
 Change the string literal below to "green" instead of "red"
 */
-const greenLength = "green".length
+const GREEN_LENGTH = "green".length
 
 checkAnswer('1.1');
 
@@ -14,7 +14,7 @@ checkAnswer('1.1');
 1.2
 Set the variable below equal to the string literal's length (using the length property)
 */
-const blueLength = "blue".length;
+const BLUE_LENGTH = "blue".length;
 
 checkAnswer('1.2');
 
@@ -22,34 +22,35 @@ checkAnswer('1.2');
 1.3
 Set the variable below equal to the length of the string "yellow"
 */
-const yellowLength = "yellow".length;
+const YELLOW_LENGTH = "yellow".length;
 
 checkAnswer('1.3');
 
 /*
 1.4
-Create a variable called orangeLength and set it equal to the length of the string "orange"
+Create a variable called ORANGE_LENGTH and set it equal to the length of the string "orange"
 */
-const orangeLength = "orange".length;
+const ORANGE_LENGTH = "orange".length;
+
 checkAnswer('1.4');
 
 /*
 1.5
-- Set purpleColor equal to "purple"
-- Set purpleLength equal to the length of the purpleColor variable
+- Set PURPLE_COLOR equal to "purple"
+- Set PURPLE_LENGTH equal to the length of the PURPLE_COLOR variable
 */
-const purpleColor = "purple";
-const purpleLength = purpleColor.length;
+const PURPLE_COLOR = "purple";
+const PURPLE_LENGTH = PURPLE_COLOR.length;
 
 checkAnswer('1.5');
 
 /*
 1.6
-- Create a variable called brownColor and set it equal to "brown"
-- Create a variable called brownLength and set it equal to the length of the brownColor variable
+- Create a variable called BROWN_COLOR and set it equal to "brown"
+- Create a variable called BROWN_LENGTH and set it equal to the length of the BROWN_COLOR variable
 */
-const brownColor = "brown";
-const brownLength = brownColor.length;
+const BROWN_COLOR = "brown";
+const BROWN_LENGTH = BROWN_COLOR.length;
 
 checkAnswer('1.6');
 
@@ -59,69 +60,77 @@ String Indexing
 
 /*
 2.1
-Change the index below so that greenLetter equals 'r'
+Change the index below so that GREEN_LETTER equals 'r'
 */
-const greenLetter = "green"[1];
+
+const GREEN_LETTER = "green"[1];
 
 checkAnswer('2.1');
 
 /*
 2.2
-Change the index below so that blueLetter equals 'u'
+Change the index below so that BLUE_LETTER equals 'u'
 */
-const blueColor = "blue";
-const blueLetter = blueColor[2];
+
+const BLUE_COLOR = "blue";
+const BLUE_LETTER = BLUE_COLOR[2];
 
 checkAnswer('2.2');
 
 /*
 2.3
-Change the index below so that pinkLetter equals 'k'
+Change the index below so that PINK_LETTER equals 'k'
 */
-const pinkColor = "pink";
-const pinkLetter = pinkColor[3];
+
+const PINK_COLOR = "pink";
+const PINK_LETTER = PINK_COLOR[3];
 
 checkAnswer('2.3');
 
 /*
 2.4
 - Go ahead and uncomment and run this test case without changing anything
-- What is the value of tealLetter? Can you explain why it's that value?
-- Change the index so that tealLetter equals the last letter in tealColor
+- What is the value of TEAL_LETTER? Can you explain why it's that value?
+- Change the index so that TEAL_LETTER equals the last letter in TEAL_COLOR
 */
-const tealColor = "teal";
-const tealLetter = tealColor[3];
+
+const TEAL_COLOR = "teal";
+const TEAL_LETTER = TEAL_COLOR[3];
 
 checkAnswer('2.4');
 
 /*
 2.5
-We're trying to find the last letter in indigoColor, but our index isn't quite right.
-- Can you update the index value so that it equals the last index in indigoColor?
+We're trying to find the last letter in INDIGO_COLOR, but our index isn't quite right.
+- Can you change the 3 in the index assignment so that INDIGO_LETTER is "o"?
 */
-const indigoColor = "indigo";
-const indigoIndex = indigoColor.length - 1;
-const indigoLetter = indigoColor[indigoIndex];
+
+const INDIGO_COLOR = "indigo";
+const INDIGO_INDEX = INDIGO_COLOR.length - 1;
+const INDIGO_LETTER = INDIGO_COLOR[INDIGO_INDEX];
 
 checkAnswer('2.5');
 
 /*
 2.6
-- Set burgundyLetter equal to the last character in burgundyColor (use the length property!)
+- Set BERGUNDY_LETTER equal to the last character in BERGUNDY_COLOR (use the length property!)
 */
-const burgundyColor = "burgundy";
-const burgundyLetter = burgundyColor[burgundyColor.length - 1];
+
+const BERGUNDY_COLOR = "burgundy";
+const BERGUNDY_LETTER = BERGUNDY_COLOR[BERGUNDY_COLOR.length - 1];
 
 checkAnswer('2.6');
 
 /*
 2.7
 Let's find the second-to-last letter in "turquoise"
-- Create a variable called turquoiseColor and set it equal to "turquoise"
-- Create a variable called turquoiseLetter and set it equal to the second-to-last letter in turquoiseColor (use the length property!)
+- Create a variable called TURQUISE_COLOR and set it equal to "turquoise"
+- Create a variable called TURQUISE_LETTER and set it equal to the second-to-last letter in TURQUISE_COLOR (use the length property!)
 */
-let turquoiseColor = "turquoise"
-let turquoiseLetter = turquoiseColor[turquoiseColor.length -2]
+
+const TURQUISE_COLOR = "turquoise";
+const TURQUISE_LETTER =  TURQUISE_COLOR[TURQUISE_COLOR.length - 2];
+
 checkAnswer('2.7');
 
 /* ______________________ PART 3 ______________________
@@ -130,33 +139,40 @@ Bonus
 
 /*
 3.1
-Let's build "sqillup" using just the letters from the colors below.
-- We've added the "s" so far. Can you append the remaining letters? (use compound assignment!)
+Let's build "codeblocks" using just the letters from the colors below.
+- We've added the "c" so far. Can you append the remaining letters? (use compound assignment, not concatenation!)
+- Feel free to experiment with using the length property in the indexes!
 */
 
-const aquamarineColor = "aquamarine";
-const periwinkleColor = "periwinkle";
-const silverColor = "silver";
+const SKY_BLUE_COLOR = "sky blue"
+const PEACH_COLOR = "peach";
+const GOLD_COLOR = "gold"
 
 let programName = "";
-programName += silverColor[0];
-programName += aquamarineColor[1];
-programName += periwinkleColor[3];
-programName += silverColor[2];
-programName += silverColor[2];
-programName += aquamarineColor[2];
-programName += periwinkleColor[0];
+programName += PEACH_COLOR[3];
+programName += GOLD_COLOR[1];
+programName += GOLD_COLOR[3];
+programName += SKY_BLUE_COLOR[SKY_BLUE_COLOR.length - 1];
+programName += SKY_BLUE_COLOR[4];
+programName += SKY_BLUE_COLOR[5];
+programName += GOLD_COLOR[1];
+programName += PEACH_COLOR[3];
+programName += SKY_BLUE_COLOR[1];
+programName += SKY_BLUE_COLOR[0];
 
 checkAnswer('3.1');
 
 /*
 3.2
-Let's build "square" using just the letters from the same colors as above. But this time, let's use simple string concatenation (no compound assignment)
-- Create a variable called companyName
-- Using string concatenation, combine letters from the strings above to get companyName equal to "square"
+Let's build "square" using just the letters from the colors below. But this time, let's use simple string concatenation (no compound assignment)
+- Create a variable called orgName
+- Using string concatenation, combine letters from the strings above to get orgName equal to "square"
 */
 
-const companyName = silverColor[0] + aquamarineColor[1] + aquamarineColor[2] + aquamarineColor[0] + periwinkleColor[2] + silverColor[4]
+const AQUAMARINE_COLOR = "aquamarine";
+const SILVER_COLOR = "silver";
+
+let orgName = SILVER_COLOR[0] + AQUAMARINE_COLOR[1] + AQUAMARINE_COLOR[2] + AQUAMARINE_COLOR[3] + AQUAMARINE_COLOR[6] + AQUAMARINE_COLOR[AQUAMARINE_COLOR.length - 1];
 
 checkAnswer('3.2');
 
@@ -166,23 +182,24 @@ We want violetLetter to equal "o", but our index isn't quite right.
 - Using either compound assignment or the increment/decrement operator, can you update the index so that violetLetter is "o"?
 */
 
-const violetColor = "violet";
-let violetIndex = 2;
+const VIOLET_COLOR = "violet";
+let violetIndex = 1;
+violetIndex++;
 
-const violetLetter = violetColor[violetIndex];
+const violetLetter = VIOLET_COLOR[violetIndex];
 
 checkAnswer('3.3');
 
 /*
 3.4
 We want lavenderLetter to equal "n", but our index isn't quite right.
-- Can you change the 1 in the index assignement so that lavenderLetter is "n"?
+- Can you change the 1 in the index assignment so that lavenderLetter is "n"?
 */
 
-const lavenderColor = "lavender";
-const lavenderIndex = lavenderColor.length - 4;
+const LAVENDER_COLOR = "lavender";
+const LAVENDER_INDEX = LAVENDER_COLOR.length - 4;
 
-const lavenderLetter = lavenderColor[lavenderIndex];
+const lavenderLetter = LAVENDER_COLOR[LAVENDER_INDEX];
 
 checkAnswer('3.4');
 
@@ -239,24 +256,24 @@ function isUninitializedVariable(variableName) {
 function checkAnswer(exercise) {
     switch (exercise) {
         case '1.1':
-            checkVariable('greenLength', 'green'.length);
+            checkVariable('GREEN_LENGTH', 'green'.length);
             break;
         case '1.2':
-            checkVariable('blueLength', 'blue'.length);
+            checkVariable('BLUE_LENGTH', 'blue'.length);
             break;
         case '1.3':
-            checkVariable('yellowLength', 'yellow'.length);
+            checkVariable('YELLOW_LENGTH', 'yellow'.length);
             break;
         case '1.4':
-            checkVariable('orangeLength', 'orange'.length);
+            checkVariable('ORANGE_LENGTH', 'orange'.length);
             break;
         case '1.5':
-            checkVariable('purpleColor', 'purple')
-            checkVariable('purpleLength', purpleColor.length);
+            checkVariable('PURPLE_COLOR', 'purple')
+            checkVariable('PURPLE_LENGTH', PURPLE_COLOR.length);
             break;
         case '1.6':
-            checkVariable('brownColor', 'brown');
-            checkVariable('brownLength', brownColor.length);
+            checkVariable('BROWN_COLOR', 'brown');
+            checkVariable('BROWN_LENGTH', BROWN_COLOR.length);
             break;
         case '1.7':
             checkVariable('', '');
@@ -265,43 +282,43 @@ function checkAnswer(exercise) {
             checkVariable('', '');
             break;
         case '2.1':
-            checkVariable('greenLetter', "green"[1]);
+            checkVariable('GREEN_LETTER', "green"[1]);
             break;
         case '2.2':
-            checkVariable('blueLetter', blueColor[2]);
+            checkVariable('BLUE_LETTER', BLUE_COLOR[2]);
             break;
         case '2.3':
-            checkVariable('pinkLetter', pinkColor[3]);
+            checkVariable('PINK_LETTER', PINK_COLOR[3]);
             break;
         case '2.4':
-            checkVariable('tealLetter', tealColor[3]);
+            checkVariable('TEAL_LETTER', TEAL_COLOR[3]);
             break;
         case '2.5':
-            checkVariable('indigoIndex', indigoColor.length - 1);
-            checkVariable('indigoLetter', indigoColor[indigoIndex]);
+            checkVariable('INDIGO_INDEX', INDIGO_COLOR.length - 1);
+            checkVariable('INDIGO_LETTER', INDIGO_COLOR[INDIGO_INDEX]);
             break;
         case '2.6':
-            checkVariable('burgundyLetter', burgundyColor[burgundyColor.length - 1]);
+            checkVariable('BERGUNDY_LETTER', BERGUNDY_COLOR[BERGUNDY_COLOR.length - 1]);
             break;
         case '2.7':
-            checkVariable('turquoiseColor', 'turquoise');
-            checkVariable('turquoiseLetter', turquoiseColor[turquoiseColor.length - 2]);
+            checkVariable('TURQUISE_COLOR', 'turquoise');
+            checkVariable('TURQUISE_LETTER', TURQUISE_COLOR[TURQUISE_COLOR.length - 2]);
             break;
         case '2.8':
             checkVariable('', '');
             break;
         case '3.1':
-            checkVariable('programName', 'sqillup');
+            checkVariable('programName', 'codeblocks');
             break;
         case '3.2':
-            checkVariable('companyName', 'square');
+            checkVariable('orgName', 'square');
             break;
         case '3.3':
             checkVariable('violetIndex', 2);
-            checkVariable('violetLetter', violetColor[violetIndex]);
+            checkVariable('violetLetter', VIOLET_COLOR[violetIndex]);
             break;
         case '3.4':
-            checkVariable('lavenderLetter', lavenderColor[lavenderColor.length - 4]);
+            checkVariable('lavenderLetter', LAVENDER_COLOR[LAVENDER_COLOR.length - 4]);
             break;
         case '3.5':
             checkVariable('', '');
